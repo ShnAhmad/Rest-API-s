889fb4f8ee2c40f20138f9580d8c2d63303920fc
Zeeshan Ahmad <zishanahmad114477@gmail.com>
2024-08-11 07:32:56 +0500

    comments api itegrated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:rest_apis/Models/comments_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,14 +12,54 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<CommentsModel> commentsList = [];
+
+  Future<List<CommentsModel>> getCommentsApi() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/comments'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      commentsList.clear();
+      for (Map<String, dynamic> i in data) {
+        commentsList.add(CommentsModel.fromJson(i));
+      }
+      return commentsList;
+    } else {
+      return commentsList;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Rest Apis'),
       ),
-      body: const Column(
-        children: [],
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getCommentsApi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Text('Loading');
+                } else {
+                  return ListView.builder(
+                      itemCount: commentsList.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                            child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Email\n${commentsList[index].email}'),
+                          ],
+                        ));
+                      });
+                }
+              },
+            ),
+          )
+        ],
       ),
     );
   }
