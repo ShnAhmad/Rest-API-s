96064fb05c405634f3e780052abaec896bfa74ac
Zeeshan Ahmad <zishanahmad114477@gmail.com>
2024-08-13 08:17:27 +0500

    integrated product api

--- lib/Screens/product_screen.dart
+++ lib/Screens/product_screen.dart
@@ -31,15 +31,72 @@ class _ProductScreenState extends State<ProductScreen> {
         ),
         body: Column(
           children: [
-            FutureBuilder(
-                future: getProductApi(),
-                builder: (context, AsyncSnapshot<ProductModel> snapshot) {
-                  if (!snapshot.hasData) {
-                    return const Text("loading");
-                  } else {
-                    return const Text('data');
-                  }
-                })
+            Expanded(
+              child: FutureBuilder(
+                  future: getProductApi(),
+                  builder: (context, AsyncSnapshot<ProductModel> snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Text("loading");
+                    } else {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.data!.length,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              children: [
+                                ListTile(
+                                  title: Text(snapshot
+                                      .data!.data![index].shop!.name
+                                      .toString()),
+                                  subtitle: Text(snapshot
+                                      .data!.data![index].shop!.description
+                                      .toString()),
+                                  leading: CircleAvatar(
+                                    backgroundImage: NetworkImage(snapshot
+                                        .data!.data![index].shop!.image
+                                        .toString()),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.height * .3,
+                                  width: MediaQuery.of(context).size.width * 1,
+                                  child: ListView.builder(
+                                      scrollDirection: Axis.horizontal,
+                                      itemCount: snapshot
+                                          .data!.data![index].images!.length,
+                                      itemBuilder: (context, index) {
+                                        return Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Container(
+                                            height: MediaQuery.of(context)
+                                                    .size
+                                                    .height *
+                                                .25,
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                .5,
+                                            decoration: BoxDecoration(
+                                              image: DecorationImage(
+                                                fit: BoxFit.cover,
+                                                image: NetworkImage(snapshot
+                                                    .data!
+                                                    .data![index]
+                                                    .images![index]
+                                                    .url
+                                                    .toString()),
+                                              ),
+                                            ),
+                                          ),
+                                        );
+                                      }),
+                                ),
+                              ],
+                            );
+                          });
+                    }
+                  }),
+            )
           ],
         ));
   }
